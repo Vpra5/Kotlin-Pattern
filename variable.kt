69343f74e670b6204bea6aa10bbf1c684776e3be
Vpra5 <89732811+Vpra5@users.noreply.github.com>
2022-11-18 16:51:56 +0530

    Add files via upload

---
+++ variable.kt
@@ -0,0 +1,21 @@
+//even or odd program
+fun main() {
+    var num:Int=20
+    var result=if(num%2==0)
+        println("$num is even ")
+    else
+        println("$num is odd")
+}
+
+//largest Number Program
+/*fun main(){
+var num1=1
+var num2=2
+var num3=3
+if(num1>num2 && num1>num3)
+print("$num1 is greater")
+else if(num2>num1 && num2>num3)
+print("$num2 is greater")
+else
+print("$num3 is greater")
+}*/
